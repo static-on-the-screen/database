766452676d703e53234a2df904988df4e5c33241
static-on-the-screen <emilhilv@woodland.on.ca>
2025-03-20 20:02:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,31 +5,38 @@ bookname = ''
 bookcount = 5
 titlelist=[]
 authorlist=[]
+isbnlist=[]
+medialist=[]
+
+def addmedia():
+    addwindow = gui.Toplevel(root)
+    addwindow.title("Add Item")
+    addwindow.geometry("500x375")
+    addmedialabel= gui.Label(addwindow, text="Add Item", justify="center", font=("Times New Roman", 20, "bold"))
+    addmedialabel.pack()
+
 def helppage():
     print("define thissss")
 
 def viewinfo():
-    print(authorlist)
-    with open('database.txt') as f:
-        for item in scrolllist.curselection():
-            print("You have selected"+str(titlelist[item]))
     infowindow = gui.Toplevel(root)
     infowindow.title(str(titlelist[item]))
     infowindow.geometry("500x375")
     titlelabel = gui.Label(infowindow, text="Title: "+str(titlelist[item]))
     titlelabel.place(x=20, y=15)
     authorlabel = gui.Label(infowindow, text="Author Name: "+str(authorlist[item]))
-    authorlabel.place(x=20, y=30)
-
-
-
+    authorlabel.place(x=20, y=40)
+    isbnlabel = gui.Label(infowindow, text="ISBN: "+str(isbnlist[item]))
+    isbnlabel.place(x=20, y=65)
+    typelabel = gui.Label(infowindow, text="Media Type: "+str(medialist[item]))
+    typelabel.place(x=20, y=90)
 
 root = gui.Tk()
 root.title("NPLDatabase.exe")
 root.geometry("900x650")
 title1 = gui.Label(root, text="Nondescript Public Library Database", font=("Times New Roman", 25, "bold"))
 title1.pack()
-addbutton = gui.Button(root, text="Add Item", padx=5, pady=5, cursor="hand2")
+addbutton = gui.Button(root, text="Add Item", padx=5, pady=5, cursor="hand2", command=addmedia)
 addbutton.place(x=10, y=50)
 deletebutton = gui.Button(root, text="Delete Item", padx=5, pady=5, cursor="hand2")
 deletebutton.place(x=80, y=50)
@@ -56,15 +63,52 @@ for item in list:
     author = ''
     for a in range(0, len(item)):
         if item[a] == '*':
-            seperate = seperate+1
+            if seperate == 1:
+                break
+            elif seperate == 0:
+                seperate = seperate+1
+                continue
         else:
-            if seperate > 2:
+            if seperate == 0:
+                continue
+            elif seperate == 1:
+               author = (author)+str(item[a])       
+    authorlist.append(author)
+
+for item in list:
+    seperate = 0
+    isbn = ''
+    for a in range(0, len(item)):
+        if item[a] == '*':
+            if seperate == 2:
+                break
+            elif seperate == 0 or seperate == 1:
+                seperate = seperate+1
+                continue
+        else:
+            if seperate == 0 or seperate == 1:
                 continue
             elif seperate == 2:
-               author = author+str(item[a])
-            if seperate < 2:
+               isbn = (isbn)+str(item[a])
+    isbnlist.append(isbn)        
+
+for item in list:
+    seperate = 0
+    mediatype = ''
+    for a in range(0, len(item)):
+        if item[a] == '*':
+            if seperate == 3:
                 break
-        authorlist.append(author)
+            elif seperate == 0 or seperate == 1 or seperate == 2:
+                seperate = seperate+1
+                continue
+        else:
+            if seperate == 0 or seperate == 1 or seperate == 2:
+                continue
+            elif seperate == 3:
+               mediatype = (mediatype)+str(item[a])
+    medialist.append(mediatype)        
+
 scrolllist.place(x=80, y=100)
 scrollbar.config(command = scrolllist.yview)
 scrollbar.place(x=50, y=100)
